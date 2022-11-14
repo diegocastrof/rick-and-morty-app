b0b56eb0df9e2d8c0667a860cb315d5770d22fa9
Diego Castro <diego.castro@houm.com>
2022-11-14 17:05:18 -0300

    feat: add styles components config and add providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import type { AppProps } from "next/app";
+import GlobalStyle from "../styles/global";
+import { ThemeProvider } from "styled-components";
+import theme from "../styles/theme";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient());
+  return (
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <GlobalStyle />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
 }
+
+export default MyApp;
---
+++ styles/global.ts
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from "styled-components";
+import Theme from "styles/theme";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: ${Theme.fonts.font_system};
+    box-sizing: border-box;
+  }
+  body {
+    background-color: ${Theme.colors.beige};
+  }
+  a {
+  color: inherit;
+  text-decoration: none;
+  }
+  h1 {
+    font-size: ${Theme.fonts.h1};
+    font-weight: bold;
+  }
+	h2 {
+    font-size: ${Theme.fonts.h2};
+    font-weight: normal;
+  }
+	h3 {
+    font-size: ${Theme.fonts.h3};
+    font-weight: normal;
+  }
+	p {
+    font-size: ${Theme.fonts.p};
+  }
+	small {
+    font-size: ${Theme.fonts.small};
+  }
+`;
